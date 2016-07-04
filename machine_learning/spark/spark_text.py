23f32b0e9a8784c643e6fe2a63747d8c6f167f21
Maximilian Pavoni <m.pavoni@araneum.it>
2016-07-04 20:30:48 +0200

    aggiunto metodo lunghezza media frasi

--- machine_learning/spark/spark_text.py
+++ machine_learning/spark/spark_text.py
@@ -1,16 +1,7 @@
-import clean_text
 from pyspark import SparkConf, SparkContext
 import re
 import json
 from operator import add
-
-
-
-
-
-
-
-
 
 def map_red_less_used(path, num):
     conf = (SparkConf().setMaster('local').setAppName('mapred'))
@@ -41,18 +32,15 @@ def average_words_length(path):
     mapred_01 = text.flatMap(lambda line: line.split(" ")).map(lambda a: (a.encode("utf-8"), (len(a))))
     tot = mapred_01.values().sum()
     print tot/float(mapred_01.count())
-    # inp = open(path, "r")
-    # sentences = [[sentence for sentence in re.sub("[^a-zA-Z\s]"," ",clean_text.stemming(line).strip()).split(" ")]for line in inp.readlines()]
-    # words=[]
-    # for sentence in sentences:
-    #     for word in sentence:
-    #         if len(word) != 0:
-    #             words.append(word)
-    # average = sum(len(word) for word in words) / len(words)
-    #
-    #
-    # print average
+
     mapred_01.saveAsTextFile("DIOBELLO")
 
 
-average_words_length("../data/input_backup/arthur_conan_doyle.txt")
+def average_sentences_length(path):
+    conf = (SparkConf().setMaster('local').setAppName('mapred'))
+    sc = SparkContext(conf=conf)
+    text = sc.textFile(path)
+    mapred_01 = text.map(lambda line: len(line.split()))
+    print mapred_01.mean()
+
+average_sentences_length("../../data/input_backup/arthur_conan_doyle.txt")
