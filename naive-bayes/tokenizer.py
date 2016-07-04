37d54d9fa5acc4be3f423e357a7070dda7e719ca
Pierluigi Pirro <poffare@msn.com>
2016-07-04 16:14:01 +0200

    first commit

---
+++ naive-bayes/tokenizer.py
@@ -0,0 +1,12 @@
+def tokenize(text):
+    return text.lower().split(' ')
+
+
+
+#
+# def tokenize(text):
+#     res=''
+#     for line in text:
+#         for word in line.split(' '):
+#             res += word
+#     return res
