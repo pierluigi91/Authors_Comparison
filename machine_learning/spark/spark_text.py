3e5d73b1865f6b02c0b9b01ebaeee6458629b3f4
Maximilian Pavoni <m.pavoni@araneum.it>
2016-07-05 17:26:56 +0200

    metodo get spark vector

--- machine_learning/spark/spark_text.py
+++ machine_learning/spark/spark_text.py
@@ -1,7 +1,9 @@
 from pyspark import SparkConf, SparkContext
 import re
+import numpy as np
 import json
 from operator import add
+
 
 def map_red_less_used(path, num):
     conf = (SparkConf().setMaster('local').setAppName('mapred'))
@@ -14,33 +16,40 @@ def map_red_less_used(path, num):
 
     return mapred_02
 
-def map_red_count_x_occ(path, x):
-    conf = (SparkConf().setMaster('local').setAppName('mapred'))
-    sc = SparkContext(conf=conf)
+
+def hapax_dix_legomena(sc, path):
     text = sc.textFile(path)
-    mapred_01 = text.flatMap(lambda line: line.split(" ")) \
+    mapred_01 = text.flatMap(lambda line: re.sub(r"[^A-Za-z\s]", "", line).split(" ")) \
         .map(lambda word: (word.encode("utf-8"), 1)) \
         .reduceByKey(lambda a, b: a + b)
+    unique_words = len(mapred_01.countByKey())
     mapred_02 = dict(mapred_01.map(lambda (a, b): (b, a)).countByKey().items())
+    return float(mapred_02[1])/float(unique_words), float(mapred_02[2])/float(unique_words)
 
-    print mapred_02[x]
 
-def average_words_length(path):
-    conf = (SparkConf().setMaster('local').setAppName('mapred'))
-    sc = SparkContext(conf=conf)
+def average_words_length(sc, path):
     text = sc.textFile(path)
-    mapred_01 = text.flatMap(lambda line: line.split(" ")).map(lambda a: (a.encode("utf-8"), (len(a))))
+    mapred_01 = text.flatMap(lambda line: re.sub(r"[^A-Za-z\s]", "", line).split(" ")).map(lambda a: (a.encode("utf-8"), (len(a))))
     tot = mapred_01.values().sum()
-    print tot/float(mapred_01.count())
+    average_length = tot/float(mapred_01.count())
+    return average_length
 
-    mapred_01.saveAsTextFile("DIOBELLO")
+
+def average_sentences_length(sc, path):
+    text = sc.textFile(path)
+    mapred_01 = text.map(lambda line: len(re.sub(r"[^A-Za-z\s]", "", line).split()))
+    return mapred_01.mean()
 
 
-def average_sentences_length(path):
+def get_spark_vector(path):
     conf = (SparkConf().setMaster('local').setAppName('mapred'))
     sc = SparkContext(conf=conf)
-    text = sc.textFile(path)
-    mapred_01 = text.map(lambda line: len(line.split()))
-    print mapred_01.mean()
+    vector = list()
+    vector.append(hapax_dix_legomena(sc, path)[0])
+    vector.append(hapax_dix_legomena(sc, path)[1])
+    vector.append(average_words_length(sc, path))
+    vector.append(average_sentences_length(sc, path))
+    print vector
+    return np.array(vector)
 
-average_sentences_length("../../data/input_backup/arthur_conan_doyle.txt")
+get_spark_vector("/Users/Max/Desktop/prova.txt")
